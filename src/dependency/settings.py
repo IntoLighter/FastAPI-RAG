5c4ac58ce49d43ea085e32aeb9552a6f13d9aa5e
Intolighter <analekseev2016@yandex.com>
2026-09-08 14:48:47 +0700

    commit

--- src/dependency/settings.py
+++ src/dependency/settings.py
@@ -40,8 +40,6 @@ class RerankerSettings(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
         env_nested_delimiter="__",
         extra="ignore",
     )
